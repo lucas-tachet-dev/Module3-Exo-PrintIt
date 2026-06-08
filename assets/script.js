4eb98c285df61002be730ab35f4319f6ed83c19b
lucas-tachet-dev <lucastachet.dev@gmail.com>
2026-06-08 18:00:59 +0200

    adding events on arrows

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,35 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let idx = 0;
+
+let bannerImg = document.querySelector("#banner .banner-img");
+let bannerText = document.querySelector("#banner p");
+
+console.log(slides.length)
+
+const arrowLeft = document.querySelector("#banner .arrow_left");
+console.log(arrowLeft)
+arrowLeft.addEventListener("click", () => {
+	console.log("ouais ouais")
+	idx--;
+	if(idx < 0){
+		idx = (slides.length - 1);
+	}
+	console.log(idx)
+	bannerImg.src = "assets/images/slideshow/" + slides[idx].image;
+	bannerText.innerHTML = `${slides[idx].tagLine}`;
+});
+
+const arrowRight = document.querySelector("#banner .arrow_right");
+console.log(arrowRight)
+arrowRight.addEventListener("click", () => {
+	idx++;
+	if(idx >= slides.length){
+		idx = 0;
+	}
+	console.log(idx)
+	bannerImg.src = "assets/images/slideshow/" + slides[idx].image;
+	bannerText.innerHTML = `${slides[idx].tagLine}`;
+});
