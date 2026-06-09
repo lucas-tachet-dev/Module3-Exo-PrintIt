8d6c1fe2b9facee53c5b2de613ef1ba72fb7eda2
lucas-tachet-dev <lucastachet.dev@gmail.com>
2026-06-09 14:42:48 +0200

    adding function + comments

--- assets/script.js
+++ assets/script.js
@@ -21,32 +21,33 @@ let idx = 0;
 
 let bannerImg = document.querySelector("#banner .banner-img");
 let bannerText = document.querySelector("#banner p");
-let dots = document.querySelectorAll(".dot")
-
-console.log(slides.length)
-
+let dots = document.querySelectorAll(".dot");
 const arrowLeft = document.querySelector("#banner .arrow_left");
-console.log(arrowLeft)
+const arrowRight = document.querySelector("#banner .arrow_right");
+
+// fonction de changement de bannière
+function bannerChange(idx){
+	bannerImg.src = `assets/images/slideshow/${slides[idx].image}`;
+	bannerText.innerHTML = slides[idx].tagLine;
+	dots[idx].classList.add("dot_selected");
+};
+
+// click sur la flèche de gauche
 arrowLeft.addEventListener("click", () => {
-	dots[idx].classList.remove("dot_selected")
+	dots[idx].classList.remove("dot_selected");
 	idx--;
 	if(idx < 0){
 		idx = (slides.length - 1);
 	}
-	bannerImg.src = "assets/images/slideshow/" + slides[idx].image;
-	bannerText.innerHTML = `${slides[idx].tagLine}`;
-	dots[idx].classList.add("dot_selected")
+	bannerChange(idx);
 });
 
-const arrowRight = document.querySelector("#banner .arrow_right");
-console.log(arrowRight)
+// click sur la flèche de droite
 arrowRight.addEventListener("click", () => {
-	dots[idx].classList.remove("dot_selected")
+	dots[idx].classList.remove("dot_selected");
 	idx++;
 	if(idx >= slides.length){
 		idx = 0;
 	}
-	bannerImg.src = "assets/images/slideshow/" + slides[idx].image;
-	bannerText.innerHTML = `${slides[idx].tagLine}`;
-	dots[idx].classList.add("dot_selected");
+	bannerChange(idx);
 });
